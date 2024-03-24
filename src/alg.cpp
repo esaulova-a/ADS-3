26d1db1f572ceb4fbe09f9199de18c22a2875e17
esaulova-a <160412658+esaulova-a@users.noreply.github.com>
2024-03-24 20:50:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int leftPos = mid - 1;
+            while (leftPos >= 0 && arr[leftPos] == value) {
+                count++;
+                leftPos--;
+            }
+            int rightPos = mid + 1;
+            while (i < rightPos && arr[rightPos] == value) {
+                count++;
+                rightPos++;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return 0; // если ничего не найдено
 }
